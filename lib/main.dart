abddc88b07250c6c749af87e71dbe176b6516f16
Erel <tranducanhghu@gmail.com>
2023-10-27 08:23:20 +0700

    add file for 9 screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'views/login_screen.dart';
+import 'views/home_screen.dart';
+import 'views/tutor_screen.dart';
+import 'views/courses_screen.dart';
+import 'views/course_info_screen.dart';
+import 'views/explore_course_screen.dart';
+import 'views/history_screen.dart';
+import 'views/call_screen.dart';
 
 void main() {
   runApp(MaterialApp(
