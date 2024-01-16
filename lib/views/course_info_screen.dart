f08eefa15706dda1397ece5e4d8750de70171eb4
Erel <tranducanhghu@gmail.com>
2024-01-16 09:44:45 +0700

    feat!: update fetch data course by id and course info screen

--- lib/views/course_info_screen.dart
+++ lib/views/course_info_screen.dart
@@ -1,130 +1,174 @@
 import 'package:flutter/material.dart';
+import 'package:test_route/controllers/course_action.dart';
+import 'package:test_route/models/course/course_detail.dart';
+import 'package:test_route/models/course/topic.dart';
 import 'package:test_route/views/course_detail_screen.dart';
 import 'package:test_route/widgets/card.dart';
 import 'package:test_route/widgets/body.dart';
 
 class CourseInfoScreen extends StatelessWidget {
-  const CourseInfoScreen({super.key});
+  const CourseInfoScreen(this.idCourse, {super.key});
+  final String idCourse;
 
   @override
   Widget build(BuildContext context) {
-    return const MainBody(Body());
+    return MainBody(Body(idCourse));
   }
 }
 
-class Body extends StatelessWidget {
-  const Body({super.key});
+class Body extends StatefulWidget {
+  const Body(this.idCourse, {super.key});
+  final String idCourse;
+
+  @override
+  State<Body> createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  late Future<CourseDetailData> futureCourseDetail;
+
+  @override
+  void initState() {
+    super.initState();
+    futureCourseDetail = fetchCourseById(widget.idCourse);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 35),
-      child: Column(
-        children: [
-          CourseCard(
-            'Basic Conversation Topics',
-            image: 'assets/images/banner07.png',
-            description: "Gain confidence speaking about familiar topics",
-            child: GestureDetector(
-              child: Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5),
-                  color: const Color.fromRGBO(0, 113, 240, 1),
-                ),
-                child: const Text(
-                  'Discover',
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-            ),
-          ),
-          const Part(
-            'Overview',
-            children: [
-              Content(
-                'Why take this course',
-                icon: Icon(
-                  Icons.question_mark_sharp,
-                  size: 20,
-                  color: Colors.red,
-                ),
-                content:
-                    "It can be intimidating to speak with a foreigner, no matter how much grammar and vocabulary you've mastered. If you have basic knowledge of English but have not spent much time speaking, this course will help you ease into your first English conversations.",
-              ),
-              Content(
-                'What will you be able to do',
-                icon: Icon(
-                  Icons.question_mark_sharp,
-                  size: 16,
-                  color: Colors.red,
-                ),
-                content:
-                    "This course covers vocabulary at the CEFR A2 level. You will build confidence while learning to speak about a variety of common, everyday topics. In addition, you will build implicit grammar knowledge as your tutor models correct answers and corrects your mistakes.",
-              ),
-            ],
-          ),
-          const Part(
-            'Experience Level',
-            children: [
-              Content(
-                'Beginner',
-                icon: Icon(
-                  Icons.group_add_outlined,
-                  size: 20,
-                  color: Colors.blue,
-                ),
-              )
-            ],
-          ),
-          const Part(
-            'Course Length',
-            children: [
-              Content(
-                '10 topics ',
-                icon: Icon(
-                  Icons.book_outlined,
-                  size: 20,
-                  color: Colors.blue,
-                ),
-              )
-            ],
-          ),
-          const Part(
-            'List Topics',
-            children: [
-              Topic('Foods You Love', index: 1),
-              Topic('Your Job', index: 2),
-              Topic('Playing and Watching Sports', index: 3),
-              Topic('The Best Pet', index: 4),
-              Topic('Having Fun in Your Free Time', index: 5),
-              Topic('Your Daily Rountine', index: 6),
-              Topic('Childhood Memories', index: 7),
-              Topic('Your Family Members', index: 8),
-              Topic('Your Hometown', index: 9),
-              Topic('Shopping Habits', index: 10),
-            ],
-          ),
-          const Part(
-            'Suggested Tutors',
-            children: [
-              Row(
-                children: [
-                  Text(
-                    'Keegan',
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    ' More info',
-                    style: TextStyle(color: Colors.blue),
-                  ),
-                ],
-              )
-            ],
-          )
-        ],
+      child: FutureBuilder(
+        future: futureCourseDetail,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return courseDetailData(snapshot.data as CourseDetailData);
+          } else if (snapshot.hasError) {
+            return Text('${snapshot.error}');
+          }
+          return const CircularProgressIndicator();
+        },
       ),
+    );
+  }
+
+  Widget courseDetailData(CourseDetailData data) {
+    return Column(
+      children: [
+        _courseCard(data),
+        _overview(data.reason, data.purpose),
+        _experienceLevel(data),
+        _numberOfTopics(data.topics!.length),
+        _listTopics(data.topics),
+        _suggestTutors(),
+      ],
+    );
+  }
+
+  Widget _courseCard(CourseDetailData data) {
+    return CourseCard(
+      data.id,
+      title: data.name,
+      image: data.imageUrl!,
+      description: data.description,
+      child: GestureDetector(
+        child: Container(
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5),
+            color: const Color.fromRGBO(0, 113, 240, 1),
+          ),
+          child: const Text(
+            'Discover',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _overview(String? reason, String? purpose) {
+    return Part(
+      'Overview',
+      children: [
+        Content(
+          'Why take this course',
+          icon: const Icon(
+            Icons.question_mark_sharp,
+            size: 20,
+            color: Colors.red,
+          ),
+          content: reason,
+        ),
+        Content(
+          'What will you be able to do',
+          icon: const Icon(
+            Icons.question_mark_sharp,
+            size: 16,
+            color: Colors.red,
+          ),
+          content: purpose,
+        ),
+      ],
+    );
+  }
+
+  Widget _experienceLevel(CourseDetailData data) {
+    return const Part(
+      'Experience Level',
+      children: [
+        Content(
+          'Beginner',
+          icon: Icon(
+            Icons.group_add_outlined,
+            size: 20,
+            color: Colors.blue,
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _numberOfTopics(int length) {
+    return Part(
+      'Course Length',
+      children: [
+        Content(
+          '$length topics ',
+          icon: const Icon(
+            Icons.book_outlined,
+            size: 20,
+            color: Colors.blue,
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _listTopics(List<Topic>? topics) {
+    return Part('List Topics',
+        children: topics!.map((topic) {
+          return TopicInfo(topic.name!, index: topic.orderCourse);
+        }).toList());
+  }
+
+  Widget _suggestTutors() {
+    return const Part(
+      'Suggested Tutors',
+      children: [
+        Row(
+          children: [
+            Text(
+              'Keegan',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              ' More info',
+              style: TextStyle(color: Colors.blue),
+            ),
+          ],
+        )
+      ],
     );
   }
 }
@@ -192,8 +236,8 @@ class Content extends StatelessWidget {
   }
 }
 
-class Topic extends StatelessWidget {
-  const Topic(this.title, {this.index, super.key});
+class TopicInfo extends StatelessWidget {
+  const TopicInfo(this.title, {this.index, super.key});
 
   final String title;
   final int? index;
